0e14c41c1e2b1e61702dd143ccdef39193603748
igorhenrique-gif <igorhffreitas2008@gmail.com>
2025-11-17 14:23:25 -0300

    Atualizado StatusUsuarioEnum

--- src/main/java/br/com/etechas/etecfood/enums/StatusUsuarioEnum.java
+++ src/main/java/br/com/etechas/etecfood/enums/StatusUsuarioEnum.java
@@ -6,4 +6,17 @@ public enum StatusUsuarioEnum {
     ATIVO,
     INATIVO,
     BLOQUEADO;
+    
+    public String getDescricao() {
+        switch (this) {
+            case ATIVO:
+                return "Usuário ativo";
+            case INATIVO:
+                return "Usuário inativo";
+            case BLOQUEADO:
+                return "Usuário bloqueado";
+            default:
+                return "Status desconhecido";
+        }
+    }
 }
