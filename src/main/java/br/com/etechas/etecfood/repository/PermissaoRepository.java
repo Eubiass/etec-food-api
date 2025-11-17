1e04abb255d289bec23e94694e2dc37ba35b7fbc
igorhenrique-gif <igorhffreitas2008@gmail.com>
2025-11-16 21:18:56 -0300

    Comentário Explicativo na interface PermissaoRepository
    comentários explicativos na interface PermissaoRepository

--- src/main/java/br/com/etechas/etecfood/repository/PermissaoRepository.java
+++ src/main/java/br/com/etechas/etecfood/repository/PermissaoRepository.java
@@ -2,6 +2,7 @@ package br.com.etechas.etecfood.repository;
 
 import br.com.etechas.etecfood.entity.Permissao;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+// Interface responsável por fornecer métodos de CRUD para a entidade Permissao
+// Herdando métodos padrão do JpaRepository
 public interface PermissaoRepository extends JpaRepository<Permissao, Long> {
 }
