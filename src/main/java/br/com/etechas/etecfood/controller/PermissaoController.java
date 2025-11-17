d83dc79ba6ceb67881c8696e886e9e2c16f34ed9
Gustavo Bastos Beloni <gustavobastos2008ssp@gmail.com>
2025-11-17 11:23:07 -0300

    classe PermissaoController configurada

---
+++ src/main/java/br/com/etechas/etecfood/controller/PermissaoController.java
@@ -0,0 +1,49 @@
+package br.com.etechas.etecfood.controller;
+
+import br.com.etechas.etecfood.entity.Usuario;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import br.com.etechas.etecfood.repository.UsuarioRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+
+@RestController
+@RequestMapping("/usuario")
+// Classe responsável por gerenciar as requisições relacionadas aos usuários.
+public class UsuarioController {
+    @Autowired
+// Repositório usado para operações de CRUD com a entidade Usuario
+    private UsuarioRepository usuarioRepository;
+}
+@GetMapping
+public List<Usuario> listar(){
+    return UsuarioRepository.findAll();
+}
+
+@GetMapping("/{id}")
+public Usuario buscarPorId(@PathVariable Long id){
+    var usuario = UsuarioRepository.findById(id);
+    if(usuario.isPresent())
+        return usuario.get();
+    return null;
+}
+
+@PostMapping
+public void cadastrar(@RequestBody Usuario usuario){
+    UsuarioRepository.save(usuario);
+}
+
+@DeleteMapping("/{id}")
+public void deletar(@PathVariable Long id){
+    var usuario = UsuarioRepository.findById(id);
+    if(usuario.isPresent())
+        UsuarioRepository.delete(usuario.get());
+}
+}
